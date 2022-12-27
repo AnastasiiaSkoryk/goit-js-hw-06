2363ea47152ae07e21a7669ee8d2762c61675496
AnastasiiaSkorik <skorik.anastasiia.97@gmail.com>
2022-12-27 21:09:10 +0200

    add js

---
+++ js/task-02.js
@@ -0,0 +1,23 @@
+const ingredients = [
+  'Potatoes',
+  'Mushrooms',
+  'Garlic',
+  'Tomatos',
+  'Herbs',
+  'Condiments',
+];
+
+
+const allIngredientsEl = document.querySelector("#ingredients")
+
+const ingredientsEl = ingredients.map(ingredient => {
+  const ingredientEl = document.createElement("li");
+  ingredientEl.classList = "item";
+  ingredientEl.textContent = ingredient;
+
+  return ingredientEl;
+});
+
+console.log(ingredientsEl);
+
+allIngredientsEl.append(...ingredientsEl);
---
+++ js/task-09.js
@@ -0,0 +1,15 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+
+const bodyEl = document.querySelector('body');
+const spanEl = document.querySelector('.color');
+const buttonEl = document.querySelector('.change-color');
+buttonEl.addEventListener('click', onButtonClick);
+function onButtonClick(event) {
+  const changeColor = getRandomHexColor();
+  bodyEl.style.backgroundColor = changeColor;
+  spanEl.textContent = changeColor;
+};
+console.log(spanEl)
